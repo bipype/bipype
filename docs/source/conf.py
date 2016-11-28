1de1ea7a047e2921b8a51a2219da2d78be08f4f6
krassowski <krassowski.michal@gmail.com>
2016-11-28 07:57:31 +0100

    TUrn off adding module names

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,8 @@ extensions = [
     'sphinx.ext.napoleon'
 ]
 
+add_module_names = False
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
